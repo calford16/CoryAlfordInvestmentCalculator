bd5674e152b6c91d9f70920cb399004762cc3b3f
Cory Alford <calford4@my.westga.edu>
2016-03-04 12:23:24 -0800

    Added test cases and the calculateInvestment method in the model class

--- app/src/main/java/edu/westga/coryalfordinvestmentcalculator/model/InvestmentCalculator.java
+++ app/src/main/java/edu/westga/coryalfordinvestmentcalculator/model/InvestmentCalculator.java
@@ -14,24 +14,48 @@ public class InvestmentCalculator {
         this.payment = 0;
     }
 
+    public int getNumberOfPayments() {
+        return this.numberOfPayments;
+    }
+
+    public double getRate() {
+        return this.rate;
+    }
+
+    public double getPayment() {
+        return this.payment;
+    }
+
     public void setNumberOfPayments(int num) {
         if (num < 0) {
             this.numberOfPayments = 0;
+        } else {
+            this.numberOfPayments = num;
         }
-        this.numberOfPayments = num;
     }
 
     public void setRate(double rate) {
         if (rate < 0) {
             this.rate = 0;
+        } else {
+            this.rate = rate / 100;
         }
-        this.rate = rate;
     }
 
     public void setPayment(double pay) {
         if (pay < 0) {
             this.payment = 0;
+        } else {
+            this.payment = pay;
         }
-        this.payment = pay;
+    }
+
+    public double calculateInvestment() {
+        if (this.rate == 0) {
+            return this.payment * this.numberOfPayments;
+        }
+
+        double futureValue = Math.pow(1 + this.rate, this.numberOfPayments) - 1;
+        return this.payment * futureValue / this.rate;
     }
 }
--- app/src/test/java/edu/westga/coryalfordinvestmentcalculator/InvestmentCalculatorTest.java
+++ app/src/test/java/edu/westga/coryalfordinvestmentcalculator/InvestmentCalculatorTest.java
@@ -1,6 +1,10 @@
 package edu.westga.coryalfordinvestmentcalculator;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.westga.coryalfordinvestmentcalculator.model.InvestmentCalculator;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +12,71 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class InvestmentCalculatorTest {
+    InvestmentCalculator calc;
+
+    @Before
+    public void setUp() {
+        this.calc = new InvestmentCalculator();
+    }
+
+    @After
+    public void tearDown() {
+        this.calc = null;
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void unsetCalculatorReturnsZero() throws Exception {
+        assertEquals(0, calc.calculateInvestment(), 0.01);
+    }
+
+    @Test
+    public void negativeValuesSetToZero() throws Exception {
+        calc.setPayment(-1000);
+        assertEquals(0, calc.getPayment(), 0.01);
+
+        calc.setRate(-1000);
+        assertEquals(0, calc.getRate(), 0.01);
+
+        calc.setNumberOfPayments(-1000);
+        assertEquals(0, calc.getNumberOfPayments());
+    }
+
+    @Test
+    public void zeroRateCalculationReturnsSimpleProduct() throws Exception {
+        calc.setNumberOfPayments(3);
+        calc.setPayment(1000);
+        assertEquals(3000, calc.calculateInvestment(), 0.01);
+    }
+
+    @Test
+    public void unrealisticValueCalculation1() throws Exception {
+        calc.setNumberOfPayments(5);
+        calc.setPayment(-1000);
+        calc.setRate(3);
+        assertEquals(0, calc.calculateInvestment(), 0.05);
+    }
+
+    @Test
+    public void unrealisticValueCalculation2() throws Exception {
+        calc.setNumberOfPayments(-2);
+        calc.setPayment(1000);
+        calc.setRate(3);
+        assertEquals(0, calc.calculateInvestment(), 0.05);
+    }
+
+    @Test
+    public void realisticValueCalculation1() throws Exception {
+        calc.setNumberOfPayments(5);
+        calc.setPayment(1000);
+        calc.setRate(3);
+        assertEquals(5309.14, calc.calculateInvestment(), 0.05);
+    }
+
+    @Test
+    public void realisticValueCalculation2() throws Exception {
+        calc.setNumberOfPayments(6);
+        calc.setPayment(731.5);
+        calc.setRate(3.62);
+        assertEquals(4805.90, calc.calculateInvestment(), 0.05);
     }
 }
